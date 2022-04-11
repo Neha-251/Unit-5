683e2f3ec6eacecd51352146741cd7d279e28bcb
Neha Sen <neha712513@gmail.com>
2022-04-11 20:44:40 +0530

    ok

--- Sprint-1/day2-react-mobile-os/src/App.js
+++ Sprint-1/day2-react-mobile-os/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
+  const opearting = ["Android","Blackberry","iPhone","Windows Phone"];
+  const company = ["Samsung","HTC","Micromax","Apple"];
+
+  return(
+    <div className='App'>
+      <h3>Mobile Operating System</h3>
+      <ul>
+        {
+          opearting.map((e)=>(
+            <li>{e}</li>
+          ))
+        }
+      </ul>
+      <h3>Mobile Manufacturers</h3>
+      <ul>
+        {
+          company.map((e)=>(
+            <li>{e}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
