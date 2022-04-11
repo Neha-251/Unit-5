951802dccbcf129c6ee86ca0d487ded76d092daa
Neha Sen <neha712513@gmail.com>
2022-04-11 20:31:00 +0530

    ok

--- Sprint-1/day3-react-counter/src/App.js
+++ Sprint-1/day3-react-counter/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(2);
+
+  const handleChange = (value)=>{
+    setCount(count+value);
+  }
+
+  const handleDouble = (value)=>{
+    setCount(count*value);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h3>Counter:{count}</h3>
+      <button onClick={()=>{
+        if(count>=20){
+          return;
+        }
+        // counter++;
+        handleChange(1)   //handle called here
+        console.log("updating")
+      }}>Add</button>
+
+      <button onClick={()=>{
+        // counter++;
+        handleChange(-1) 
+        console.log("updating")
+      }}>Substract</button>
+
+      <button onClick={()=>{
+        // counter++;
+        handleDouble(2) 
+        console.log("updating")
+      }}>Double</button>
+      <div className={count % 2 === 0 ? 'green' : 'red'}>Number is:{count%2===0 ? "Even": "Odd"}</div>
+      
     </div>
   );
 }
